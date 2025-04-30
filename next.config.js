385f3c2c195000883a50c491739b430f44bf4b85
johnmaikelima <johnpatrocinio1@gmail.com>
2025-04-30 20:24:44 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     appDir: true,
   },
