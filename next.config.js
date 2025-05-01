83643a93987017b28aa07d093cc421f1bff621ea
johnmaikelima <johnpatrocinio1@gmail.com>
2025-04-30 22:13:41 -0300

    Adicionado cartacapital.com.br aos domínios permitidos para imagens

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     domains: [
       'www.lomadee.com',
-      'www.google.com'
+      'www.google.com',
+      'www.cartacapital.com.br'
     ],
   },
   webpack: (config) => {
